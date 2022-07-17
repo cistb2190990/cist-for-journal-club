3f2a73ae661dd4552e54cfa6a0e6dd7e149e96e5
cistb2190990 <82741593+cistb2190990@users.noreply.github.com>
2022-07-17 20:40:23 +0900

    入力用インターフェースをとりあえず作成

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <title>ここにサービスのタイトル</title>
+    </head>
+    <body>
+        <nav class="navbar bg-light">
+            <div class="container-fluid">
+                <h1 class="navbar-brand mb-0">ここにサービスのタイトル</h1>
+            </div>
+        </nav>
+        <div class="container my-3">
+            <form>
+                <div class="mb-3">
+                    <input type="text" class="form-control" id="text" placeholder="ここにテキストを入力…" aria-describedby="textHelp">
+                    <div id="textHelp" class="form-text">字数制限とかなんかそういうのあれば</div>
+                </div>
+                <button type="submit" class="btn btn-primary">送信</button>
+            </form>
+            <h2>使い方</h2>
+            <ol>
+                <li>必要であれば…</li>
+            </ol>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    </body>
+</html>
